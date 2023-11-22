194bf431d4664578405dd6f0d829a68ec372e882
elen04ka05 <elena.d.ezhova@yandex.ru>
2023-11-22 17:35:37 +0300

    First homework done

---
+++ _2_java.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class _2_java {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str;
+        for (; scan.hasNext(); ) {
+            str = scan.next();
+            System.out.print(str + " ");
+        }
+    }
+}
